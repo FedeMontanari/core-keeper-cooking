8d8d6136dee59c9840e7b4e3fa6963486d312ff1
FedeMontanari <fedemontanari@outlook.com>
2024-08-23 18:56:45 -0300

    Added scraping script and edited README

---
+++ scraping/index.ts
@@ -0,0 +1,55 @@
+import fs from "node:fs";
+import Cookbook from "./Cookbook.json";
+import Buffs from "./BuffIDs.json";
+
+type CookbookType = {
+  name: string;
+  stats: string;
+  variation: number;
+};
+
+type BuffType = {
+  name: string;
+  slug: string;
+};
+
+function parseCookbook() {
+  const arr: CookbookType[] = [];
+
+  Cookbook.forEach((it) => {
+    if (arr.some((v) => v.name === it.name)) return;
+    arr.push(it);
+  });
+
+  try {
+    fs.writeFileSync("./cookbook-out.json", JSON.stringify(arr), "utf-8");
+  } catch (err) {
+    console.error(err);
+    throw new Error("Error parsing Cookboon.json");
+  }
+}
+
+function parseBuffs() {
+  const arr: BuffType[] = [];
+  Buffs.forEach((it) => {
+    const slug = it.name.toLowerCase().replaceAll(" ", "_");
+    arr.push({
+      name: it.name,
+      slug,
+    });
+  });
+
+  try {
+    fs.writeFileSync("./buffs-out.json", JSON.stringify(arr), "utf-8");
+  } catch (err) {
+    console.error(err);
+    throw new Error("Error parsing Cookboon.json");
+  }
+}
+
+function main() {
+  parseCookbook();
+  parseBuffs();
+}
+
+main();
